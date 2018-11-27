a048c9e7b52e354e6a36338763eccd8421851bd0
bcnickerson <bcnickerson@wpi.edu>
2018-11-27 10:35:40 -0500

    Caching for prices and images

--- multi-cloud/app/gather_prices.py
+++ multi-cloud/app/gather_prices.py
@@ -3,16 +3,35 @@ from libcloud.compute.providers import get_driver
 from datetime import timedelta
 import pickle
 
-
 # AWS Authentication Keys
-EC2_ACCESS_ID = 'your key'
-EC2_SECRET_KEY = 'your secret'
+EC2_ACCESS_ID = ''
+EC2_SECRET_KEY = ''
+AWS_PRICE_FILE = "app/aws_sizes.txt"
+
+
+# AWS Amazon Machine Images
+# Access using aws_images["linux"]
+aws_images = {
+    "linux": "ami-09479453c5cde9639",
+    "win": "ami-05f5c28bb6992ab4b",
+    "rhel": "ami-011b3ccf1bd6db744",
+    "unix": "ami-0ac019f4fcb7cb7e6"
+}
+
+# GCP OS Images
+gcp_images = {
+    "linux": "sles-12-sp2-sap-v20180816",
+    "win": "sql-2014-standard-windows-2012-r2-dc-v20181113",
+    "rhel": "rhel-6-v20181113",
+    "unix": "ubuntu-1404-trusty-v20181114"
+}
 
 # Google Cloud Platform Authentication Keys
-GCP_EMAIL = 'your email'
-GCP_PROJECT_ID = 'your project'
-GCP_CLIENT_ID = 'your client'
-GCP_SECRET_KEY = 'your secret'
+GCP_EMAIL = ''
+GCP_PROJECT_ID = ''
+GCP_CLIENT_ID = ''
+GCP_SECRET_KEY = ''
+GCP_PRICE_FILE = "app/gcp_sizes.txt"
 
 margin = timedelta(days = 10)
 
@@ -21,17 +40,67 @@ def gather_prices():
 
     # AWS Connection
     cls = get_driver(Provider.EC2)
-    aws_driver = cls(EC2_ACCESS_ID, EC2_SECRET_KEY)
+    aws_driver = cls(EC2_ACCESS_ID, EC2_SECRET_KEY, region="us-east-1")
     aws_sizes = aws_driver.list_sizes()
 
     # Google Cloud Connection
     ComputeEngine = get_driver(Provider.GCE)
-    gcp_driver = ComputeEngine(GCP_CLIENT_ID, GCP_SECRET_KEY,
-                           project=GCP_PROJECT_ID)
+    gcp_driver = ComputeEngine(GCP_CLIENT_ID, GCP_SECRET_KEY, project=GCP_PROJECT_ID, datacenter="us-east1", auth_type="IA")
     gcp_sizes = gcp_driver.list_sizes()
 
     write_prices_to_file("aws_sizes.txt", aws_sizes)
     write_prices_to_file("gcp_sizes.txt", gcp_sizes)
+
+
+
+def gather_images():
+    # AWS Connection
+    cls = get_driver(Provider.EC2)
+    aws_driver = cls(EC2_ACCESS_ID, EC2_SECRET_KEY, region="us-east-1")
+    aws_image_list = aws_driver.list_images()
+
+    images = {
+        "linux": None,
+        "win": None,
+        "rhel": None,
+        "unix": None
+    }
+
+    for i in aws_image_list:
+        if i.id == aws_images["linux"]:
+            images["linux"] = i
+        if i.id == aws_images["win"]:
+            images["win"] = i
+        if i.id == aws_images["rhel"]:
+            images["rhel"] = i
+        if i.id == aws_images["unix"]:
+            images["unix"] = i
+
+    # Google Cloud Connection
+    ComputeEngine = get_driver(Provider.GCE)
+    gcp_driver = ComputeEngine(GCP_CLIENT_ID, GCP_SECRET_KEY, project=GCP_PROJECT_ID, auth_type="IA")
+    gcp_image_list = gcp_driver.list_images()
+
+    images_gcp = {
+        "linux": None,
+        "win": None,
+        "rhel": None,
+        "unix": None
+    }
+
+    for i in gcp_image_list:
+        if i.id == gcp_images["linux"]:
+            images_gcp["linux"] = i
+        if i.id == gcp_images["win"]:
+            images_gcp["win"] = i
+        if i.id == gcp_images["rhel"]:
+            images_gcp["rhel"] = i
+        if i.id == gcp_images["unix"]:
+            images_gcp["unix"] = i
+
+    write_prices_to_file("aws_images.txt", images)
+    write_prices_to_file("gcp_images.txt", images_gcp)
+
 
 
 def write_prices_to_file(filename, sizes):
@@ -39,11 +108,9 @@ def write_prices_to_file(filename, sizes):
     pickle.dump(sizes, f)
     f.close()
 
+
 def read_prices_from_file(filename):
     f = open(filename, 'r')
     sizes = pickle.load(f)
     f.close()
     return sizes
-
-
-
